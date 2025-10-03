96380ff9dcb6a5dafc4f9ad2d45379c06011ba06
Cory Loeffelman <hyperplexed.email@gmail.com>
2025-10-03 10:46:05 -0500

    Removed extra logic

--- src/utilities/cell.ts
+++ src/utilities/cell.ts
@@ -4,9 +4,5 @@ import { rotation } from "$src/utilities/rotation";
 import type { DigitKey } from "$src/types/digit";
 
 export const cell = (value: string, index: number) => {
-  const digit = digits[value as DigitKey][index];
-
-  if(digit) return digit;
-
-  return rotation[" "];
+  return digits[value as DigitKey][index] || rotation[" "];
 }
