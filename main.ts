320cc036dc781fa611d3cbec0d4c0449c9d28e7d
Ethan Wang <ethanwang314159@gmail.com>
2025-10-25 10:58:05 +1100

    speed insights

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 import { app } from "$src/components/app";
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
+injectSpeedInsights();
 inject();
 app.init();
